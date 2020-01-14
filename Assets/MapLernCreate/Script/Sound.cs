f16b6313c1d3439590f6e901574b8a66b87fd62f
kbataNtsuki <konatsu1118@icloud.com>
2020-01-14 21:44:19 +0900

    マージ

---
+++ Assets/MapLernCreate/Script/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    // オーディオソースの取得
+    [SerializeField]
+    private AudioSource audio;
+    [SerializeField]
+    private AudioClip se;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
