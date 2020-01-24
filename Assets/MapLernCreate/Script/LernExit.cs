f7777ac2f3d6abfec93e814dab3441cb46bd5811
kbataNtsuki <konatsu1118@icloud.com>
2020-01-24 19:57:37 +0900

    アセットの追加とバグの解消

---
+++ Assets/MapLernCreate/Script/LernExit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LernExit : MonoBehaviour
+{
+    // プレイヤーのタグ取得
+    const string PLAYER = "player";
+
+
+    // プレイヤーが当たったらゲームオーバーにする
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "player")
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("GameOver");
+            //SceneManager.LoadScene("Gameover");
+        }
+    }
+}
